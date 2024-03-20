f051a1622892c498075d2f86ad88b9e88fe6516b
TheCodeRizzler <163922371+TheCodeRizzler@users.noreply.github.com>
2024-03-20 15:35:47 -0500

    added .idea files

---
+++ script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, text me 7633003902! ;)";
+  gif.src =
+    "https://media.tenor.com/bWUeVRqW9-IAAAAi/fast-cat-cat-excited.gif";
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+  createSparkles();
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
